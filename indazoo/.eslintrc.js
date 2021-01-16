017b9a20bb388bf6712073aa52dc372a40bf401f
bkkoli <ryubokgi@gmail.com>
2021-01-16 17:52:18 +0900

    modify settings

---
+++ indazoo/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'airbnb', // or airbnb-base
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended', // 설치 한경우
+    // 'plugin:import/errors', // 설치한 경우
+    // 'plugin:import/warnings', // 설치한 경우
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    semi: [2, 'never'],
+    'no-console': ['warn', { allow: ['debug', 'warn', 'info', 'error'] }],
+    'no-unused-vars': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        parser: 'typescript',
+        trailingComma: 'es5',
+        semi: false,
+        printWidth: 160,
+        arrowParens: 'avoid',
+      },
+      {
+        usePrettierrc: false,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
