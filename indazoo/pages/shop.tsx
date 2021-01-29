3c1c710c2c0133aad517966b157c8e9fade77114
bkkoli <ryubokgi@gmail.com>
2021-01-30 02:55:11 +0900

    make layout sample page

---
+++ indazoo/pages/shop.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import React from 'react'
+import IndazooLayout from '../system/organisms/IndazooLayout'
+
+export default function Home() {
+  return (
+    <IndazooLayout title="SHOP">
+      <div>Comming Soon</div>
+    </IndazooLayout>
+  )
+}
