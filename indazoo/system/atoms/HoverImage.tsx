1da8cd17f8490685bd4ef47221eeff99da04f49a
YeongSoo Kim <2presentis@naver.com>
2021-01-31 14:04:41 +0900

    HoverImage Changing Test

--- indazoo/system/atoms/HoverImage.tsx
+++ indazoo/system/atoms/HoverImage.tsx
@@ -1,20 +1,26 @@
 import React, { useState, useRef, memo } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 type HoverImageProps = {
 	src: string;
+	width: number;
+	height: number;
 };
 
 HoverImage.defaultProps = {
 	src: '',
+	width: 100,
+	height: 100
 };
 
-function HoverImage({ src }: HoverImageProps) {
+function HoverImage({ src, width, height }: HoverImageProps) {
 	const [isHovering, setIsHovering] = useState(false);
 
 	const handleHover = () => setIsHovering(!isHovering);
 
 	return (
-		<img src={src} onMouseEnter={handleHover} onMouseLeave={handleHover} />
+		<img src={src} width={isHovering ? width * 2 : width} height={height} onMouseEnter={handleHover} onMouseLeave={handleHover} />
 	);
 }
 
